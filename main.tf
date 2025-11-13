0aa87e3c5d3543b51a33cc66307151a2126446b5
vishnu1805 <vishnusenthilkumar26@gmail.com>
2025-11-13 16:46:18 +0530

    modification

--- main.tf
+++ main.tf
@@ -99,6 +99,15 @@ resource "aws_security_group" "web_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+  description = "Allow app traffic (port 8081)"
+  from_port   = 8081
+  to_port     = 8081
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+
   tags = { Name = "webapp-sg" }
 }
 
