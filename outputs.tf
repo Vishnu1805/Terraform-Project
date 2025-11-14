7e0aa849bbf71431b93ac175ba317ea74d28f400
vishnu1805 <vishnusenthilkumar26@gmail.com>
2025-11-14 11:38:31 +0530

    modification

--- outputs.tf
+++ outputs.tf
@@ -3,11 +3,6 @@ output "ec2_public_ip" {
   value       = aws_instance.web_server.public_ip
 }
 
-output "ec2_public_dns" {
-  description = "Public DNS of the EC2 instance"
-  value       = aws_instance.web_server.public_dns
-}
-
 output "load_balancer_dns" {
   description = "Public DNS name of the Load Balancer"
   value       = aws_lb.web_alb.dns_name
